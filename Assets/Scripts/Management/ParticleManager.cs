3130cf2c7e0e8be40565cc102faadf4379282aa6
Tyson Thomas <tysonmthomas@gmail.com>
2023-06-29 23:52:10 -0400

    VS Fix
    -Fixed error where VS wouldn't highlight errors

--- Assets/Scripts/Management/ParticleManager.cs
+++ Assets/Scripts/Management/ParticleManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ParticleManager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
