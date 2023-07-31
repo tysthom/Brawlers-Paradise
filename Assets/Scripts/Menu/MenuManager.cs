67f9bbc1aceb118d3a8f3560417b8f61d43be2e6
Tyson Thomas <tysonmthomas@gmail.com>
2023-07-30 23:58:06 -0400

    Menu Updates
    -Added Black Out Image
    -Added new camera
    -Made Title Screen fade out

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    [Header("Main Menu References")]
+    public bool canMoveToNextMenu;
+    public string currentMenu = "";
+    public GameObject blackOut;
+
+    [Header("Cameras")]
+    public GameObject titleCamera;
+    public GameObject mainMenuCamera;
+
+    [Header("Title Menu References")]
     public GameObject mainLogo;
     public GameObject startText;
 
@@ -12,6 +20,7 @@ public class MenuManager : MonoBehaviour
     {
         mainLogo.GetComponent<CanvasGroup>().alpha = 0;
         startText.GetComponent<CanvasGroup>().alpha = 0;
+        currentMenu = "Title Menu";
     }
 
     // Start is called before the first frame update
@@ -20,7 +29,24 @@ public class MenuManager : MonoBehaviour
         StartCoroutine(MainLogoTime(2));
 
     }
-    
+
+    private void Update()
+    {
+        bool nextMenu = Input.GetButton("Pick Up");
+        if (canMoveToNextMenu && nextMenu)
+        {
+            if (currentMenu == "Title Menu")
+            {
+                mainLogo.GetComponent<Fade>().fadeOut = true;
+                startText.GetComponent<Fade>().fadeOut = true;
+                StartCoroutine(BlackOut(1, 3));
+                StartCoroutine(SwitchCameras(2, titleCamera, mainMenuCamera));
+            }
+
+            canMoveToNextMenu = false;
+        }
+    }
+
 
     IEnumerator MainLogoTime(float t)
     {
@@ -28,6 +54,23 @@ public class MenuManager : MonoBehaviour
         mainLogo.GetComponent<Fade>().fadeIn = true;
         yield return new WaitForSeconds(t);
         startText.GetComponent<Fade>().fadeIn = true;
-
+        yield return new WaitForSeconds(1.5f);
+        canMoveToNextMenu = true;
     }
+
+    IEnumerator BlackOut(float waitTime, float blackOutTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        blackOut.GetComponent<Fade>().fadeIn = true;
+        yield return new WaitForSeconds(blackOutTime);
+        blackOut.GetComponent<Fade>().fadeOut = true;
+    }
+
+    IEnumerator SwitchCameras(float waitTime, GameObject currentCamera, GameObject newCamera)
+    {
+        yield return new WaitForSeconds(waitTime);
+        newCamera.SetActive(true);
+        currentCamera.SetActive(false);
+    }
+
 }
